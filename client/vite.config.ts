f7ae121ff23288046d76a3bf90b5147b05eff44c
Ander Aginaga / Laquin <anderaginaga2@gmail.com>
2024-03-07 14:24:35 +0100

    Adapt server proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: 'localhost',
+    port: 5000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      }
+    }
+  }
 })
